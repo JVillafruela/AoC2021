4699a742c05d432ee1b27f29781d3e46df03b830
Jérôme <jvillafruela@users.noreply.github.com>
2021-12-01 22:20:26 +0100

    day 01

---
+++ day01/main.go
@@ -0,0 +1,44 @@
+package main
+
+import (
+	"aoc2021/utils"
+	"fmt"
+)
+
+func main() {
+	part1("input.txt")
+	part2("input.txt")
+}
+
+func part1(filename string) {
+	ints := utils.ReadInts(filename)
+
+	prev := 0
+	increase := 0
+	for _, num := range ints {
+		if prev > 0 && num > prev {
+			increase++
+		}
+		prev = num
+	}
+	fmt.Println("part1", increase)
+}
+
+func part2(filename string) {
+	ints := utils.ReadInts(filename)
+
+	increase := 0
+	for i := range ints {
+		if i < 3 {
+			continue
+		}
+		if window(ints, i) > window(ints, i-1) {
+			increase++
+		}
+	}
+	fmt.Println("part2", increase)
+}
+
+func window(a []int, i int) int {
+	return a[i-2] + a[i-1] + a[i]
+}
